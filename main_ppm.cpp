eb8e3431fd1c0df07a6350f260739442abd8afe3
Hubert Weissmann <hu.bert@xemail.me>
2013-02-13 20:17:59 +0100

    little changes, few new files and ready Projektarbeit

--- main_ppm.cpp
+++ main_ppm.cpp
@@ -1,7 +1,7 @@
 #include"ppm.h"
 
 int main(){
-   Ppm foo(80, 40, 50000, 12.8, 6.8, 0.015, 0.015, 900); // Ppm(int prey, int predator, double time, float konst1, float konst2, float konst21, float konst12, int run){
+   Ppm foo(250, 25, 50000, 12.8, 7.1, 0.015, 0.015, 900); // Ppm(int prey, int predator, double time, float konst1, float konst2, float konst21, float konst12, int run){
    foo.gillespie();
 //   foo.print();
    foo.evaluation(); 
